55c7f53e7358de864ea2c851d24c99cba7aad218
RobinKrederDeltion <97043715@st.deltion.nl>
2017-11-10 12:34:33 +0100

    unity scene

---
+++ Unity scene/World Machine/Assets/Explosie/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    public float timer = 3.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform.GetComponent<Renderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= 1 * Time.deltaTime;
+        
+        if(timer < 0)
+        {
+            transform.GetComponent<Renderer>().enabled = true;
+        }
+	}
+}
